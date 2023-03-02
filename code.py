51a86e430a74588f9c29b8af6f6f72c32f2e2690
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-03-02 22:00:06 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+class Solution:
+    def summaryRanges(self, nums):
+        ranges = []
+        for n in nums:
+            if not ranges or n > ranges[-1][-1] + 1:
+                ranges += [],
+            ranges[-1][1:] = n,
+        return ['->'.join(map(str, r)) for r in ranges]
+
+
+        #Just collect the ranges, then format and return them. O(n) time
+        
+
+ 
+
+
+
